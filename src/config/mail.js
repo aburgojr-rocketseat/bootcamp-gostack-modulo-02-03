699aa21c915be2f3f659c0595e47ca3f0b06415c
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2020-02-12 23:03:13 -0300

    add appointment cancellation and send email

---
+++ src/config/mail.js
@@ -0,0 +1,21 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '6f0e05a38b2921',
+    pass: 'bebf1852e4cee2',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+// Serviços de Email
+
+// Amazon SES
+// Mailgun
+// Sparkpost
+// Mandril (Mailchimp)
+// Gmail
+// Mailtrap(DEV)
